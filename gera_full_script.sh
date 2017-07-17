2be64937f6ab05965d318a0b13351c3be9ef05bd
DionisiusMayr <dionisiusom@gmail.com>
2017-07-17 19:46:55 -0300

    gera_full_script com views, functions e triggers

--- gera_full_script.sh
+++ gera_full_script.sh
@@ -3,3 +3,6 @@
 cat drop_tables.sql > script_completo.sql
 cat create_tables_final.sql >> script_completo.sql
 cat insert_into.sql >> script_completo.sql
+cat ./views/*.sql >> script_completo.sql
+cat ./functions/*.sql >> script_completo.sql
+cat ./triggers/*.sql >> script_completo.sql
---
+++ views/plano_saude_paciente.sql
@@ -0,0 +1,6 @@
+-- Autor: Dionisius Mayr
+------------------------------------------------------------------------------------------------------------------------
+-- O conjunto de usuários Paciente necessita gerenciar:
+-- O uso de seus planos de saúde, com informes sobre consultas/exames que tenham sido pagos pelos planos,
+-- informando qual o plano de saúde, qual o número do cartão de associado ao plano e data/hora nos quais foi utilizado;
+
